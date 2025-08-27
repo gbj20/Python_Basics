4aafbd606edac2021654aaab2ff78ce1ed1bffc4
Gayatri Bajirao Jadhav <117456242+gbj20@users.noreply.github.com>
2025-08-27 16:15:56 +0530

    Delete List functions and methods directory

--- List functions and methods/swap_elements.py
+++
@@ -1,7 +0,0 @@
-'''Write a program that swaps the first and last elements of a given list'''
-
-a = [10, 20, 30, 40, 50]
-
-
-a[0], a[-1] = a[-1], a[0]
-print("List after swapping first and last elements:", a)
